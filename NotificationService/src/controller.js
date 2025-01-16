504ffe95816a4554b3e07e51d23a0129a05cd6d9
leandro cavalcante <contato@leandrw.com.br>
2025-01-15 22:34:33 -0300

    feat: notification service

---
+++ NotificationService/src/controller.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const router = express.Router();
+
+/** @type {string[]} */
+const notifications = [];
+
+router.get('/notifications', (req, res) => {
+    try {
+        return res.status(200).send({ notifications });
+    } catch (error) {
+        return res.status(500).send({ message: error.message });
+    }
+});
+
+export default router;
---
+++ NotificationService/src/index.js
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+
+import express from 'express';
+import fs from "fs";
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yaml';
+import path from 'path';
+
+import controller from './controller.js';
+
+const app = express();
+
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+const file = fs.readFileSync(path.join(import.meta.dirname, './openapi.yaml'), 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+app.listen(port, () => {
+    console.log(`NotificationService listening at ${host}:${port}`);
+    console.log(`Swagger documentation available at ${host}:${port}/docs`);
+});
+
+app.use(express.json());
+app.use('/', controller);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
