4e5d9e606ee1af6e568e1c97ce365492fae96fcc
leandro cavalcante <contato@leandrw.com.br>
2025-01-19 23:32:38 -0300

    fix

--- training-service/src/app.js
+++ training-service/src/app.js
@@ -4,7 +4,7 @@ import fs from "fs"
 import express from "express"
 import swaggerUi from 'swagger-ui-express'
 import YAML from 'yaml'
-import controller from './controller.js'
+import controller from './Controller.js'
 
 const app = express()
 
